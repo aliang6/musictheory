e8c520de6186412c36f5d78f60c1440143acddee
Andy Liang <andy.liang.1@stonybrook.edu>
2016-12-04 22:24:59 -0500

    Created a helper function in the Python File

--- musicTheory.py
+++ musicTheory.py
@@ -1,7 +1,5 @@
 #! usr/bin/python
 
-
-
 x = 2
 x = "lsdkjfkdlsjfjk"
 
@@ -12,12 +10,34 @@ y = [1]*5 # [1,1,1,1,1]
 
 y[1] = 2
 
-print y
+print (y)
 y.append(9)
 
-print y
+print (y)
 
 def po ( y, u, p= None):
 
 	pass
-print x 
+print (x) 
+
+accidentals = []
+noteDictionary = []
+notes = []
+perfectInterval = []
+majorInterval = []
+
+accidentals = ["bb", "b", "n", "#", "x"]
+noteDictionary = ["GA", "A", "AB", "B", "C", "CD", "D", "DE", "E", "F", "FG", "G", "GA", "A", "AB", "B", "C", "CD", "D", "DE", "E", "F", "FG", "G"]
+notes = ["A", "B", "C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G"]
+perfectInterval = ["d", "P", "A"]
+majorInterval = ["d", "m", "M", "A"]
+
+
+#Helper Functions===========================
+def findNote (note):
+	for x in notes:
+		if x == note:
+			print (x)
+			return x
+			break
+
